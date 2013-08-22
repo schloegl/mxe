324ee6553c3fa509a599afcc45d5b0ceb90a406a
Alois Schlögl <alois.schloegl@ist.ac.at>
2013-08-22 10:58:57 +0200

    pkg x264: fix built - revert part related to x264 of commit 9ccca928de4f1a1c81fbe14b11ad834843378196

--- src/x264.mk
+++ src/x264.mk
@@ -28,7 +28,3 @@ define $(PKG)_BUILD
     $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)'
     $(MAKE) -C '$(BUILD_DIR)' -j 1 install
 endef
-
-$(PKG)_BUILD_i686-pc-mingw32 =
-$(PKG)_BUILD_i686-w64-mingw32 =
-$(PKG)_BUILD_x86_64-w64-mingw32 =
