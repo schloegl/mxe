d73bc216532873971171b7f4a88f4f21079fb510
Alois Schlögl <alois.schloegl@ist.ac.at>
2016-01-30 10:42:18 +0100

    pkg biosig: upgrade to v1.7.4

--- src/stimfit.mk
+++ src/stimfit.mk
@@ -3,11 +3,13 @@
 
 PKG             := stimfit
 $(PKG)_IGNORE   := 
-$(PKG)_CHECKSUM := 61d3cbf049468b6a714acc5a6f451945c9d3f0a8
+$(PKG)_VERSION  := 0.14.5windows
+$(PKG)_CHECKSUM := a0e0c25238be4070c698b13fecadc4f14dd6023f89181f5fbdb805cce9e09c32
+#b4484ff4bde473b89f74b55114fa2bec3accf95a
 $(PKG)_SUBDIR   := stimfit-$($(PKG)_VERSION)
 $(PKG)_FILE     := stimfit-$($(PKG)_VERSION).tar.gz
-$(PKG)_URL      := https://stimfit.googlecode.com/files/$($(PKG)_FILE)
-$(PKG)_DEPS     := gcc libbiosig wxwidgets hdf5 boost fftw
+$(PKG)_URL      := https://github.com/neurodroid/stimfit/archive/refs/tags/v$($(PKG)_VERSION).tar.gz
+$(PKG)_DEPS     := gcc biosig wxwidgets hdf5 boost fftw
 
 define $(PKG)_UPDATE
     wget -q -O- 'https://github.com/neurodroid/stimfit/releases' | \
