014a4a0b01509b2cfb6cdd95cb1b7da782425274
Alois SCHLOEGL <alois.schloegl@ist.ac.at>
2022-12-11 12:39:20 +0100

    apr-util: 1.6.1

--- src/apr-util.mk
+++ src/apr-util.mk
@@ -4,13 +4,13 @@ PKG             := apr-util
 $(PKG)_WEBSITE  := https://apr.apache.org/
 $(PKG)_DESCR    := APR-util
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.6.0
-$(PKG)_CHECKSUM := 483ef4d59e6ac9a36c7d3fd87ad7b9db7ad8ae29c06b9dd8ff22dda1cc416389
+$(PKG)_VERSION  := 1.6.1
+$(PKG)_CHECKSUM := b65e40713da57d004123b6319828be7f1273fbc6490e145874ee1177e112c459
 $(PKG)_SUBDIR   := apr-util-$($(PKG)_VERSION)
 $(PKG)_FILE     := apr-util-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := https://archive.apache.org/dist/apr/$($(PKG)_FILE)
 $(PKG)_URL_2    := https://www.apache.org/dist/apr/$($(PKG)_FILE)
-$(PKG)_DEPS     := cc apr expat libiconv
+$(PKG)_DEPS     := cc apr expat libiconv freetds
 
 define $(PKG)_UPDATE
     $(WGET) -q -O- 'https://apr.apache.org/download.cgi' | \
@@ -24,7 +24,6 @@ define $(PKG)_BUILD
         --without-pgsql \
         --without-sqlite2 \
         --without-sqlite3 \
-        --without-freetds \
         --with-apr='$(PREFIX)/$(TARGET)'
     $(MAKE) -C '$(1)' -j '$(JOBS)' $(MXE_DISABLE_CRUFT) LDFLAGS=-no-undefined
     $(MAKE) -C '$(1)' -j 1 install $(MXE_DISABLE_CRUFT)
